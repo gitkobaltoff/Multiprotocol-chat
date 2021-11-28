55e567f9622d9a8f8bdbcc8decc95d0554545439
gitkobaltoff <40690145+gitkobaltoff@users.noreply.github.com>
2021-11-28 23:22:25 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Network Programming
+## Lab 2 - Multiprotocol chat
+### Run `server.bat` to start server
+### Run `client.bat` to start client
---
+++ client.py
@@ -0,0 +1,77 @@
+import socket
+import select
+import errno
+import sys
+
+HEADERSIZE = 10
+IP = "127.0.0.1"
+PORT = 1234
+
+my_username = input("Username: ")
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((IP, PORT))
+client_socket.setblocking(False)
+
+username = my_username.encode('utf-8')
+username_header = f"{len(username):<{HEADERSIZE}}".encode('utf-8')
+client_socket.send(username_header+username)
+
+while True:
+    message = input(f"{my_username} > ")
+    #message = ""
+
+    if message:
+        message = message.encode('utf-8')
+        message_header = f"{len(message) :< {HEADERSIZE}}".encode('utf-8')
+        client_socket.send(message_header + message)
+
+    try:
+        while True:
+            # receive
+            username_header = client_socket.recv(HEADERSIZE)
+            if not len(username_header):
+                print("connection closed by the server")
+                sys.exit()
+
+            username_length = int(username_length.decode('utf-8').strip())
+            username = client_socket.recv(username_length).decode('utf-8')
+
+            message_header = client_socket.recv(HEADERSIZE)
+            message_length = int(message_header.decode('utf-8').strip())
+            message = client_socket.recv(message_length).decode('utf-8')
+
+            print(f"{username} > {message}")
+
+    except IOError as e:
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+            print('Reading error', str(e))
+            sys.exit()
+        continue
+
+    except Exception as e:
+        print('General error', str(e))
+        sys.exit()
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 1234))
+
+while True:
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(16)
+        if new_msg:
+            print(f"new message lentgh: {msg[:HEADERSIZE]}")
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False
+
+        full_msg += msg.decode("utf-8")
+
+        if len(full_msg)-HEADERSIZE == msglen:
+            print("full msg recvd")
+            print(full_msg[HEADERSIZE:])
+            new_msg = True
+            full_msg = ''
+
+    print(full_msg)
